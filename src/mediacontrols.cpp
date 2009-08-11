769273cb47fc242370b35cfbb5ad0537dd842b9d
Casey Jones <jonescaseyb@gmail.com>
2009-08-10 19:46:34 -0500

    Fixed kajamtag so that extern "C" is no longer needed to include kajamtag.h

--- src/mediacontrols.cpp
+++ src/mediacontrols.cpp
@@ -22,11 +22,8 @@
 
 //don't put kajamtag.h at the end of includes,
 //mainwindow.cpp thinks it defined k_tags first; it didn't.
-//I don't know why it thinks it did, or why putthing this at the top works
-extern "C" {
+//I don't know why it thinks it did, or why putting this at the top works
 #include <kajamtag/kajamtag.h>
-}
-
 #include "headers/mediacontrols.h"
 
 MediaControls::MediaControls(QWidget *parent) : QWidget(parent)
