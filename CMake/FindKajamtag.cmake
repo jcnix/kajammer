658d46fded154fea9fbd5e5fedd5e0983d166458
Casey Jones <jonescaseyb@gmail.com>
2009-09-10 21:00:43 -0500

    cmake: can now find kajamtag.h in it's new place.

--- CMake/FindKajamtag.cmake
+++ CMake/FindKajamtag.cmake
@@ -1,4 +1,4 @@
-FIND_PATH( Kajamtag_INCLUDE_DIR kajamtag.h /usr/include /usr/include/kajamtag /usr/local/include/kajamtag )
+FIND_PATH( Kajamtag_INCLUDE_DIR kajamtag/kajamtag.h /usr/include )
 
 FIND_LIBRARY( Kajamtag_LIBRARY NAMES kajamtag PATH /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64 ) 
 
@@ -7,9 +7,7 @@ IF ( Kajamtag_INCLUDE_DIR AND Kajamtag_LIBRARY )
 ENDIF ( Kajamtag_INCLUDE_DIR AND Kajamtag_LIBRARY )
 
 IF( Kajamtag_FOUND )
-   IF( NOT Kajamtag_FIND_QUIETLY )
-      MESSAGE( STATUS "Found Kajamtag: ${Kajamtag_LIBRARY}" )
-   ENDIF( NOT Kajamtag_FIND_QUIETLY )
+    MESSAGE( STATUS "Found Kajamtag: ${Kajamtag_LIBRARY}" )
 ELSE( Kajamtag_FOUND )
    IF( Kajamtag_FIND_REQUIRED )
 	  MESSAGE( FATAL_ERROR "Could not find Kajamtag" )
