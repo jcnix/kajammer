b9840fb86aa2cb37977fe13cfb45385b7299da5a
Casey Jones <jonescaseyb@gmail.com>
2009-10-14 18:48:00 -0500

    Bump to v0.9.2.2

--- src/menubar.cpp
+++ src/menubar.cpp
@@ -153,7 +153,7 @@ void MenuBar::showCollectionScanner()
 void MenuBar::aboutDialog()
 {
     QMessageBox::information(this, ("About KaJammer"),
-                            "KaJammer Media Player 0.9.2.1\n"
+                            "KaJammer Media Player 0.9.2.2\n"
                             "Licensed under the GPLv3.\n\n");
 }
 
