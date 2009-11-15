a993bc778daa3cb94c0832cbc20390a87a445145
Casey Jones <jonescaseyb@gmail.com>
2009-11-14 21:30:45 -0600

    New AboutDialog class

---
+++ src/dialogs/AboutDialog.cpp
@@ -0,0 +1,33 @@
+/*
+ * File:   AboutDialog.cpp
+ * Author: Casey Jones
+ *
+ * Created on November 13, 2009, 11:44 PM
+ *
+ * This file is part of KaJammer.
+ *
+ * KaJammer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KaJammer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KaJammer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "AboutDialog.h"
+
+AboutDialog::AboutDialog()
+{
+    init();
+}
+
+void AboutDialog::init()
+{
+    setWindowTitle("About KaJammer");
+}
---
+++ src/dialogs/AboutDialog.h
@@ -0,0 +1,45 @@
+/*
+ * File:   AboutDialog.h
+ * Author: Casey Jones
+ *
+ * Created on November 13, 2009, 11:44 PM
+ *
+ * This file is part of KaJammer.
+ *
+ * KaJammer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KaJammer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KaJammer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _ABOUTDIALOG_H
+#define _ABOUTDIALOG_H
+
+#include <QtGui/QDialog>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QVBoxLayout>
+
+#include "../kajammer.h"
+
+class AboutDialog : public QDialog
+{
+    Q_OBJECT;
+    
+public:
+    AboutDialog();
+    
+private:
+    void init();
+    
+    QIcon *icon
+};
+
+#endif //_ABOUTDIALOG_H
