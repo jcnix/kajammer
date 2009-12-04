9401e1b2868a62b3b18a3bd16b738196bd5585c8
Casey Jones <jonescaseyb@gmail.com>
2009-12-03 22:05:06 -0600

    initial CollectionManager
    This commit mostly adds Sqlite3 to the build system.

---
+++ src/sql/CollectionManager.cpp
@@ -0,0 +1,30 @@
+/*
+ * File:   ColletionManager.cpp
+ * Author: Casey Jones
+ *
+ * Created on December 3, 2009, 8:38 PM
+ *
+ * This file is part of KaJammer.
+ *
+ * KaJammer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KaJammer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KaJammer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "CollectionManager.h"
+
+CollectionManager::CollectionManager() {
+    const char* db = DB_PATH.toStdString().c_str();
+    sqlite3 *pDB;
+    
+    int status = sqlite3_open(db, &pDB);
+}
---
+++ src/sql/CollectionManager.h
@@ -0,0 +1,39 @@
+/*
+ * File:   ColletionManager.h
+ * Author: Casey Jones
+ *
+ * Created on December 3, 2009, 8:38 PM
+ *
+ * This file is part of KaJammer.
+ *
+ * KaJammer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KaJammer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KaJammer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _COLLECTIONMANAGER_H
+#define _COLLECTIONMANAGER_H
+
+#include <QtCore/QObject>
+#include <QtCore/QDir>
+#include <sqlite3.h>
+
+#define DB_PATH QString(QDir::homePath()+"/.kajammer/kj.db")
+
+class CollectionManager : public QObject {
+    Q_OBJECT;
+    
+public:
+    CollectionManager();
+};
+
+#endif //_COLLECTIONMNAGER_H
