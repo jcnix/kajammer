12eb6570a175ccc96fa67fb1520ceb40452870ba
Casey Jones <jonescaseyb@gmail.com>
2009-12-15 20:25:55 -0600

    Use the global topLayout member instead of making an accidental local member

--- src/dialogs/PlaylistEditor.cpp
+++ src/dialogs/PlaylistEditor.cpp
@@ -84,7 +84,7 @@ void PlaylistEditor::init()
     squeezeTopLayout->addWidget(btnAdd);
     squeezeTopLayout->addWidget(btnRemove);
 
-    QHBoxLayout *topLayout = new QHBoxLayout;
+    *topLayout = new QHBoxLayout;
     topLayout->addLayout(squeezeTopLayout);
 
     controlSqueezeLayout = new QHBoxLayout;
